4036cbf2c7d7f2d92fb59e0d2fa70993c384162d
Kendrick Knowles <55988094+klknowles@users.noreply.github.com>
2021-03-14 13:38:05 +0000

    Update formSignUpProcess.php

--- docuweb/scripts/formSignUpProcess.php
+++ docuweb/scripts/formSignUpProcess.php
@@ -3,17 +3,7 @@
     * This is where the code checks whether all the information inserted to are correct register it in database
 */
 // Change this to your connection info.
-$DATABASE_HOST = '127.0.0.1';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = 'knowles1970';
-$DATABASE_NAME = 'fakedb';
-// Try and connect using the info above.
-$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
-if (mysqli_connect_errno()) {
-	// If there is an error with the connection, stop the script and display the error.
-	die ('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
-
+include("../common/commonly.php");
 // Make sure the submitted registration values are not empty.
 if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
 	// One or more values are empty.
